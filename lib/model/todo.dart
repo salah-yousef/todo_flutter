a7bd3f1d13563fc2a0585601df92aca2bd70a9c7
salah_yousef <salah.yousef@pharmatolia.com>
2019-10-28 13:53:32 +0300

    init commit

---
+++ lib/model/todo.dart
@@ -0,0 +1,17 @@
+class Todo {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Todo(this._title, this._priority, this._date, [this._description]);
+  Todo.withId(this._id, this._title, this._priority, this._date,
+      [this._description]);
+
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+  int get priority => _priority;
+}
