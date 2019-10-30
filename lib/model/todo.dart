7b854d9ff3623d707cf713b28e22d49c8ef9c87f
salah_yousef <salah.yousef@pharmatolia.com>
2019-10-30 17:59:39 +0300

    add dbhelpers

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -14,4 +14,47 @@ class Todo {
   String get description => _description;
   String get date => _date;
   int get priority => _priority;
+
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      _title = newTitle;
+    }
+  }
+
+  set description(String newDescription) {
+    if (newDescription.length <= 255) {
+      _description = newDescription;
+    }
+  }
+
+  set date(String newDate) {
+    _date = newDate;
+  }
+
+  set priority(int newPriority) {
+    if (newPriority > 0 && newPriority <= 3) {
+      _priority = newPriority;
+    }
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+
+    map['title'] = _title;
+    map['description'] = _description;
+    map['priority'] = _priority;
+    map['date'] = _date;
+    if (_id != null) {
+      map['id'] = _id;
+    }
+    return map;
+  }
+
+  Todo.fromObject(dynamic o) {
+    this._id = o['id'];
+    this._title = o['title'];
+    this._description = o['description'];
+    this._priority = o['priority'];
+    this._date = o['date'];
+  }
 }
---
+++ lib/util/dbhelper.dart
@@ -0,0 +1,50 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_flutter/model/todo.dart';
+
+class DbHelper {
+  static final DbHelper _dbHelper = DbHelper._internal();
+  String tblTodo = 'todo';
+  String colId = 'id';
+  String colTitle = 'title';
+  String colDescription = 'description';
+  String colPriority = 'priority';
+  String colDate = 'date';
+
+  DbHelper._internal();
+
+  factory DbHelper() {
+    return _dbHelper;
+  }
+
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db == null) {
+      _db = await initializeDb();
+    }
+    return _db;
+  }
+
+  Future<Database> initializeDb() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path + 'todos.db';
+    var dbTodos = await openDatabase(path, version: 1, onCreate: _createDb);
+    return dbTodos;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    String sql =
+        "CREATE TABLE $tblTodo($colId INTEGER PRIMARY KEY, $colTitle TEXT,)" +
+            "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT";
+    await db.execute(sql);
+  }
+
+  Future<int> insertTodo(Todo todo) async {
+    Database db = await this.db;
+    var result = await db.insert(tblTodo, todo.toMap());
+    return result;
+  }
+}
